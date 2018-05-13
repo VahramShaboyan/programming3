f62d35f358ed82a0ca870b4002bccb786074c950
Your Name <you@example.com>
2018-05-13 13:19:19 +0400

    prog3 files are added.

--- homeworklesson3/index.js
+++ homeworklesson3/index.js
@@ -1,11 +1,9 @@
 var fs = require('fs');
 
-var obj = 
-{
-    "first_name": "Abraham",
-    "last_name": "Lincoln",
-    "age": 30,
-    "Us_president": true
-}
-var myJSON = JSON.stringify(obj);
-document.getElementById("demo").innerHTML = myJSON;
+var obj =
+    {
+        "first_name": "Abraham",
+        "last_name": "Lincoln",
+        "age": 30,
+        "Us_president": true
+    }
