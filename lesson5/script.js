e9e7796a31b940daa7cb3f85297fc523e3f2a761
Your Name <you@example.com>
2018-05-17 17:39:06 +0400

    updated

---
+++ lesson5/script.js
@@ -0,0 +1,38 @@
+function main() {
+    var socket = io.connect('http://localhost:3000');
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var del = document.getElementById('delete');
+
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+
+    function deleteMessages(evt) {
+        socket.emit("uzum em jnjem");
+
+    }
+    del.onclick = deleteMessages;
+
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+    }
+
+    socket.on('display message', handleMessage);
+    socket.on('"dzer tagery jnjeq"', deleteTags);
+
+
+    function deleteTags(){
+        
+    }
+} // main closing bracket
+
+window.onload = main;
