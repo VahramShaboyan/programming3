82bbc50847ddbebc2796bbc64efb892844606828
Your Name <you@example.com>
2018-05-20 14:22:32 +0400

    homeworkdraw is added.

--- homeworkdraw/script.js
+++ homeworkdraw/script.js
@@ -1,25 +1,25 @@
-function main() {
-    var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:3000');
 
-    function setup() {
-        createCanvas(800,800);
-        background("#BCD8FF");
-        fill("red");
-    }
+function setup() {
+    createCanvas(500, 500);
+    background("#BCD8FF");
 
-    function mouseDragged(){
-        var obj_cordinats = {
-            x: mouseX,
-            y: mouseY
-        }
-        socket.emit("send", obj_cordinats);
-    }
-
-    function draw() {
-        socket.on("send message", function(data){
-            
-        }); 
-    } 
-
+    r = random(255);
+    g = random(255);
+    b = random(255);
 }
+function mouseDragged() {
+    socket.emit("send", [mouseX, mouseY, r,g,b]);
+}
+function main() {
+    socket = io.connect('http://localhost:3000');
+
+    socket.on("sendtoall", function (cords) {
+        fill(cords[2], cords[3], cords[4])
+        ellipse(cords[0], cords[1], 30, 30);
+    });
+}
+ 
 window.onload = main;
+
+
--- homeworkdraw/server.js
+++ homeworkdraw/server.js
@@ -2,12 +2,19 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var cords = [];
 app.use(express.static("."));
 app.get('/', function (req, res) {
    res.redirect('index.html');
 });
 server.listen(3000);
 io.on('connection', function (socket) {
-
+    for (i in cords){
+        io.sockets.emit("sendtoall", cords[i]);
+    }
+    socket.on("send", function(data){
+        cords.push(data);
+        io.sockets.emit("sendtoall", data);
+    });
 });
-console.log("Hello");
+ 
